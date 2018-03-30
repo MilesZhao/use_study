d4b3efbf9270a679adf08fb25449e3d4880f916d
MilesZhao <mileszhao1990@yahoo.com>
2018-03-30 00:48:10 -0400

    Add hidden question 3

--- questions.php
+++ questions.php
@@ -11,7 +11,7 @@ include 'header.php';
 	<div class="container">
 	  <h2>General Questions</h2>
 	  <form action="./end.php" method="get">
-	  	<p>1. Do you think it is useful to extract tips for APIs?</p>
+	  	<p>Do you think it is useful to extract tips for APIs?</p>
 	    <div class="radio">
 	      <label><input type="radio" name="q1" value=1>Agree</label>
 	    </div>
@@ -19,7 +19,7 @@ include 'header.php';
 	      <label><input type="radio" name="q1" value=0>Disagree</label>
 	    </div>
 
-	    <p>2. Do you think the extracted tips from our approach are useful to you?</p>
+	    <p>Do you think the extracted tips from our approach are useful to you?</p>
 	    <div class="radio">
 	      <label><input type="radio" name="q2" value=1>Agree</label>
 	    </div>
@@ -27,15 +27,25 @@ include 'header.php';
 	      <label><input type="radio" name="q2" value=0>Disagree</label>
 	    </div>
 
-	    <p>3. If no to 2, do you think they are useful to novice developers?</p>
+	    <!-- <p>If no to 2, do you think they are useful to novice developers?</p>
 	    <div class="radio">
 	      <label><input type="radio" name="q3" value=1>Agree</label>
 	    </div>
 	    <div class="radio">
 	      <label><input type="radio" name="q3" value=0>Disagree</label>
-	    </div>
+	    </div> -->
 
-	    <p>4. Do you think our approach can outperform the baseline?</p>
+		<div id="three" hidden="true">
+     		<p>3. If no to 2, do you think they are useful to novice developers?</p >
+            <div class="radio">
+              <label><input type="radio" name="q3" value=1>Agree</label>
+            </div>
+            <div class="radio">
+              <label><input type="radio" name="q3" value=0>Disagree</label>
+            </div>
+        </div>
+
+	    <p>Do you think our approach can outperform the baseline?</p>
 	    <div class="radio">
 	      <label><input type="radio" name="q4" value=1>Agree</label>
 	    </div>
@@ -43,7 +53,7 @@ include 'header.php';
 	      <label><input type="radio" name="q4" value=0>Disagree</label>
 	    </div>
 
-	    <p>5. Do you have any recommendation/comments/thoughts?</p>
+	    <p>Do you have any recommendation/comments/thoughts?</p>
 	    <div class="form-group">
             <textarea class="form-control" rows="5" name="q5"><?php echo ($comments)?></textarea>
         </div>
@@ -51,5 +61,11 @@ include 'header.php';
 	    <button type="submit">Submit</button>
 	  </form>
 	</div>
+	<script type="application/x-javascript">
+        function showThree(show) {
+            var three = document.getElementById("three");
+            three.hidden = !show;
+        }
+    </script>
 </body>
 </html>
