9e32a38f3f8a1a7ab5fe902c4e67a5491ddc3242
MilesZhao <mileszhao1990@yahoo.com>
2018-03-30 00:54:47 -0400

    Add isset for _GET

--- end.php
+++ end.php
@@ -5,35 +5,47 @@
 	$sql = "SELECT * FROM `user_study_participant_info` where `userid`=$labeluser";
 	$result = runQuery($sql);
 	if(isset($_GET['issubmit'])){
-		$q1 = $_GET['q1'];
-		$q2 = $_GET['q2'];
-		$q3 = $_GET['q3'];
-		$q4 = $_GET['q4'];
-		$q5 = $_GET['q5'];
-		$q6 = $_GET['q6'];
+		if (isset($_GET['q1'])) {
+			$q1 = $_GET['q1'];
+			# code...
+		}
+		if (isset($_GET['q2'])) {
+			$q2 = $_GET['q2'];
+			# code...
+		}
+		if (isset($_GET['q3'])) {
+			$q3 = $_GET['q3'];
+			# code...
+		}
+		if (isset($_GET['q4'])) {
+			$q4 = $_GET['q4'];
+			# code...
+		}
+		if (isset($_GET['q5'])) {
+			$q5 = $_GET['q5'];
+			# code...
+		}
 		if(count($result)>0){
-			$sql = "UPDATE `user_study_participant_info` set `q1`=:col1,`q2`=:col2,`q3`=:col3,`q4`=:col4,`q5`=:col5 ,`q6`=:col6 where `userid` =:col7";
+			$sql = "UPDATE `user_study_participant_info` set `q1`=:col1,`q2`=:col2,`q3`=:col3,`q4`=:col4,`q5`=:col5 where `userid` =:col6";
 			$statement = $conn->prepare($sql); 
 	        $statement->bindParam(':col1', $q1);
 	        $statement->bindParam(':col2', $q2);
 	        $statement->bindParam(':col3', $q3);
 	        $statement->bindParam(':col4', $q4);
 	        $statement->bindParam(':col5', $q5);
-	        $statement->bindParam(':col6', $q6);
-	        $statement->bindParam(':col7', $labeluser);
+	        $statement->bindParam(':col6', $labeluser);
 	        $statement->execute();
 		}else{
-			$sql = "INSERT into `user_study_participant_info` (`q1`,`q2`,`q3`,`q4`,`q5`,`q6`,`userid`) values (:col1,:col2,:col3,:col4,:col5,:col6,:col7)";
+			$sql = "INSERT into `user_study_participant_info` (`q1`,`q2`,`q3`,`q4`,`q5`,`userid`) values (:col1,:col2,:col3,:col4,:col5,:col6)";
 			$statement = $conn->prepare($sql); 
 	        $statement->bindParam(':col1', $q1);
 	        $statement->bindParam(':col2', $q2);
 	        $statement->bindParam(':col3', $q3);
 	        $statement->bindParam(':col4', $q4);
 	        $statement->bindParam(':col5', $q5);
-	        $statement->bindParam(':col6', $q6);
-	        $statement->bindParam(':col7', $labeluser);
+	        $statement->bindParam(':col6', $labeluser);
 	        $statement->execute();
-		}
+		}		
 	}
 ?>
 <!DOCTYPE html>
